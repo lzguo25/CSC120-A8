1019371799c0a13b52e99b70d1edd6cf25f824dd
lzguo25 <113069414+lzguo25@users.noreply.github.com>
2022-11-16 16:21:06 -0500

    Update Pet.java

--- Pet.java
+++ Pet.java
@@ -1,17 +1,13 @@
-public class Pet extends PetShelter{
+public class Pet extends PetShelter {
 
+    public Pet(String name, int age, String animalType, String breed, String health, int timeInShelter) {
+        super(name, age, animalType, breed, health, timeInShelter);
 
-public Pet(String name, int age, String animalType, String breed, String health, int timeInShelter){
-    super(name, age, animalType, breed, health, timeInShelter);
-}
+    }
 
-
-
-
-
-public static void main(String[]args){
-Pet Tangbao = new Pet("Tangbao", 1, "Cat", "Domestic Short Hair", "Good", 1);
-System.out.println(Tangbao);
-}
+    public static void main(String[] args) {
+        Pet Tangbao = new Pet("Tangbao", 1, "Cat", "Domestic Short Hair", "Good", 1);
+        System.out.println(Tangbao);
+    }
 
 }
